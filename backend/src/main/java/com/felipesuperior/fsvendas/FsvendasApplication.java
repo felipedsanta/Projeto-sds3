1d3a137b20edd25d173872694f6fe4aefed581d0
Felipe Santos <felipe.carro3@outlook.com>
2021-05-03 13:09:21 -0300

    Projeto Criado

---
+++ backend/src/main/java/com/felipesuperior/fsvendas/FsvendasApplication.java
@@ -0,0 +1,13 @@
+package com.felipesuperior.fsvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsvendasApplication.class, args);
+	}
+
+}
