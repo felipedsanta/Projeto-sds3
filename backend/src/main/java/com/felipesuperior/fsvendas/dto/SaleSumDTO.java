6c1cf751b21da5d0f1a278a42c1d94cc1811e8d4
Felipe Santos <felipe.carro3@outlook.com>
2021-05-05 14:57:44 -0300

    Group by search

---
+++ backend/src/main/java/com/felipesuperior/fsvendas/dto/SaleSumDTO.java
@@ -0,0 +1,37 @@
+package com.felipesuperior.fsvendas.dto;
+
+import java.io.Serializable;
+
+import com.felipesuperior.fsvendas.entities.Seller;
+
+public class SaleSumDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private String SellerName;
+	private Double sum;
+	
+	public SaleSumDTO() {
+	}
+
+	public SaleSumDTO(Seller seller, Double sum) {
+		SellerName = seller.getName();
+		this.sum = sum;
+	}
+
+	public String getSellerName() {
+		return SellerName;
+	}
+
+	public void setSellerName(String sellerName) {
+		SellerName = sellerName;
+	}
+
+	public Double getSum() {
+		return sum;
+	}
+
+	public void setSum(Double sum) {
+		this.sum = sum;
+	}
+}
--- backend/src/main/java/com/felipesuperior/fsvendas/services/SaleService.java
+++ backend/src/main/java/com/felipesuperior/fsvendas/services/SaleService.java
@@ -1,5 +1,7 @@
 package com.felipesuperior.fsvendas.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.felipesuperior.fsvendas.dto.SaleDTO;
+import com.felipesuperior.fsvendas.dto.SaleSuccessDTO;
+import com.felipesuperior.fsvendas.dto.SaleSumDTO;
 import com.felipesuperior.fsvendas.entities.Sale;
 import com.felipesuperior.fsvendas.repositories.SaleRepository;
 import com.felipesuperior.fsvendas.repositories.SellerRepository;
@@ -26,5 +30,15 @@ public class SaleService {
 		sellerRepository.findAll();
 		Page<Sale> result = repository.findAll(pageable);
 		return result.map(x -> new SaleDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller(){
+		return repository.amountGroupedBySeller();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller(){
+		return repository.successGroupedBySeller();
 	}
 }
